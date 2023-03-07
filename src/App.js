ce06a2e52e14a66a8a39256e4445d49c285edb73
Hyungju Kwon <hyungju.kwon@gmail.com>
2023-03-07 13:15:01 -0500

    cleaned up App.js Die.js

--- src/App.js
+++ src/App.js
@@ -3,47 +3,7 @@ import Die from './Die'
 import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
 
-/**
- * Challenge:
- *
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components
- *   - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *   - Use flexbox on main to center the dice container
- *        in the center of the page
- */
 export default function App() {
-	/**
-	 * Challenge:
-	 * 1. Add new state called `tenzies`, default to false. It
-	 *    represents whether the user has won the game yet or not.
-	 * 2. Add an effect that runs every time the `dice` state array
-	 *    changes. For now, just console.log("Dice state changed").
-	 */
-
-	/**
-	 * Challenge: Tie off loose ends!
-	 * 1. If tenzies is true, Change the button text to "New Game"
-	 * 2. If tenzies is true, use the "react-confetti" package to
-	 *    render the <Confetti /> component 🎉
-	 *
-	 *    Hint: don't worry about the `height` and `width` props
-	 *    it mentions in the documentation.
-	 */
-
-	/**
-	 * Challenge:
-	 *
-	 * - Write a function (allNewDice) that returns an array
-	 *   of 10 random numbers between 1-6 inclusive.
-	 * - Log the array of numbers to the console for now
-	 */
-
 	const [dice, setDice] = React.useState(allNewDice())
 	const [tenzies, setTenzies] = React.useState(false)
 
@@ -108,32 +68,6 @@ export default function App() {
 			holdDice={() => holdDice(die.id)}
 		/>
 	))
-	/**
-	 * Challenge: Update the `rollDice` function to not just roll
-	 * all new dice, but instead to look through the existing dice
-	 * to NOT role any that are being `held`.
-	 *
-	 * Hint: this will look relatively similiar to the `holdDice`
-	 * function below. When creating new dice, remember to use
-	 * `id: nanoid()` so any new dice have an `id` as well.
-	 */
-
-	/**
-	 * Challenge: Allow the user to play a new game when the
-	 * button is clicked and they've already won
-	 */
-
-	/**
-	 * Challenge: Update the `holdDice` function to flip
-	 * the `isHeld` property on the object in the array
-	 * that was clicked, based on the `id` prop passed
-	 * into the function.
-	 *
-	 * Hint: as usual, there's > 1 way to accomplish this.
-	 * I'll be using `dice.map()` and checking for the `id`
-	 * of the die to determine which one to flip `isHeld` on,
-	 * but you can do whichever way makes the most sense to you.
-	 */
 
 	return (
 		<main>
