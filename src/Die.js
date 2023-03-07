a275873bc6ca9bacfd2f6929bc8fe58b857f63ea
Hyungju Kwon <hyungju.kwon@gmail.com>
2023-03-07 14:55:09 -0500

    css modified and dice images added

--- src/Die.js
+++ src/Die.js
@@ -4,7 +4,11 @@ export default function Die(props) {
 	const styles = { backgroundColor: props.isHeld ? '#59E391' : '#FFF' }
 
 	return (
-		<div className='die-face' style={styles} onClick={props.holdDice}>
+		<div
+			className={`die-face die-${props.value} ${props.isHeld ? 'is-held' : ''}`}
+			style={styles}
+			onClick={props.holdDice}
+		>
 			<h2 className='die-num'>{props.value}</h2>
 		</div>
 	)
